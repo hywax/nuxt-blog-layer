cea47288499337634b1f2012a25f39b6a96005a7
hywax <me@hywax.space>
2024-06-28 12:29:40 +0500

    chore: base layer

---
+++ src/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Error
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+</script>
